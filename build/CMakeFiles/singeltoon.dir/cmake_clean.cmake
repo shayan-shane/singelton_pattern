8ec40a21e80bdf1418eda3417e27b0b4fbbca941
shane19shayan17 <shayansetayandevolleyking5@gmail.com>
2026-07-24 23:42:16 +0330

    first initialize of cmake

---
+++ build/CMakeFiles/singeltoon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/singeltoon.dir/src/main.cpp.obj"
+  "CMakeFiles/singeltoon.dir/src/main.cpp.obj.d"
+  "libsingeltoon.dll.a"
+  "singeltoon.exe"
+  "singeltoon.exe.manifest"
+  "singeltoon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/singeltoon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
