093f2c6d7d298ac294b096384a266e876084e486
shayan-shane <setayandeshayan@gmail.com>
2026-07-26 14:31:54 +0330

    add general variable to db class

--- include/DataBase.hpp
+++ include/DataBase.hpp
@@ -1,14 +1,24 @@
 #ifndef __DATABASE_HPP__
 #define __DATABASE_HPP__
 
+#inclede <vector>
+
 class DataBase
 {
 private:
     static DataBase *db;
     DataBase();
 
+    vector<double> speed{0.0, 0.0, 0.0};
+    double steer{0.0};
+
 public:
     static DataBase *get_instance();
+
+    void set_speed(const vector<double> &new_speed);
+    double get_speed();
+    void set_steer(const double &new_steer);
+    double get_steer();
 };
 
 #endif
