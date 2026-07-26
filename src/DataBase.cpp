95a5bcc8ea8feb89e56cd09d779bb00022afb640
shayan-shane <setayandeshayan@gmail.com>
2026-07-26 14:34:18 +0330

    cpp implementation of general variables

--- src/DataBase.cpp
+++ src/DataBase.cpp
@@ -17,3 +17,20 @@ DataBase *DataBase::get_instance()
     }
     return db;
 }
+
+void DataBase::set_speed(const vector<double> &new_speed)
+{
+    speed = new_speed;
+}
+double DataBase::get_speed()
+{
+    return speed;
+}
+void DataBase::set_steer(const double &new_steer)
+{
+    steer = new_steer;
+}
+double DataBase::get_steer()
+{
+    return steer;
+}
